850748ebae0b022505be20ede5f0469085234f09
Shirm917 <shirm91799@gmail.com>
2022-10-23 20:55:17 +0300

    finished daily challenge

--- Week2/Day5/Daily-Challenge/script.js
+++ Week2/Day5/Daily-Challenge/script.js
@@ -6,13 +6,13 @@ let number = 0;
 function bottlesOfBeer () {
     bottles = prompt("How many bottles of beer should we start with?");
     for (let i = bottles; i >= 0; i -= number) {
-        if (number > i) {
+        if (i === 1) {
+            console.log(`${i} bottle of beer on the wall, ${i} bottle of beer, Take 1 down, pass it around, ${i -1} bottles of beer on the wall`);
+        } else if (number > i) {
             console.log(`${i} bottles of beer on the wall, ${i} bottles of beer, Take ${i} down, pass them around, 0 bottles of beer on the wall`);
         } else if (i > 1) {
             number += 1;
             console.log(`${i} bottles of beer on the wall, ${i} bottles of beer, Take ${number} down, pass them around, ${i - number} bottles of beer on the wall`);
-        } else if (i === 1) {
-            console.log(`${i} bottle of beer on the wall, ${i} bottle of beer, Take 1 down, pass it around, ${i -1} bottles of beer on the wall`);
         } 
     }
     console.log("0 bottles of beer on the wall");
