60003d6ee90b7c7daabcb164a5eba9574dfc956a
Shirm917 <shirm91799@gmail.com>
2022-11-20 23:31:09 +0200

    adding exercises

---
+++ Week7/Day1/Exercises/ExercisesXP/Exercise1/npm-node/main.js
@@ -0,0 +1,12 @@
+// Exercise 1 : Hello Node.Js
+// Create a new folder called - npm-node
+// Create a JS file called - main.js
+// Add the following code to the file main.js:
+const a = 5;
+const b = 10;
+// Write some more code in the main.js file so when you run it in the command prompt it logs Hello you are 15
+function greeting() {
+    console.log("Hello you are" + " " + (a + b));
+}
+
+greeting();
