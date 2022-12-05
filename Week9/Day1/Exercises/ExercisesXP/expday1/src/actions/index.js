12b1efe8550078a73e4810417f7aa1cbf302e609
Shir Markowitz <shirm91799@gmail.com>
2022-12-05 10:24:03 +0200

    adding constants

--- Week9/Day1/Exercises/ExercisesXP/expday1/src/actions/index.js
+++ Week9/Day1/Exercises/ExercisesXP/expday1/src/actions/index.js
@@ -1,11 +1,14 @@
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+
 export const INCREASE_COUNT = () => {
     return {
-        type: "INCREASE"
+        type: INCREASE
     }
 }
 
 export const DECREASE_COUNT = () => {
     return {
-        type: "DECREASE"
+        type: DECREASE
     }
 }
--- Week9/Day1/Exercises/ExercisesXP/expday1/src/reducers/index.js
+++ Week9/Day1/Exercises/ExercisesXP/expday1/src/reducers/index.js
@@ -1,12 +1,14 @@
+import {INCREASE, DECREASE} from "../actions"
+
 let initState = {
     count: 0
 }
 
 export const reducer = (state=initState,action) => {
     switch (action.type) {
-        case "DECREASE":
+        case DECREASE:
             return {...state, count: state.count - 1}
-        case "INCREASE":
+        case INCREASE:
             return {...state, count: state.count + 1}
         default:
             return {...state}
